cc1c9c3a39198d7b42fb1088eae76c2ba2830fea
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-02-08 18:28:08 -0300

    inicio proyecto

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,19 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Main {
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args)  {
 
+try {
+    URL url = new URL("https://api.openweathermap.org/geo/1.0/direct?q=AR-C&limit=5&appid=3e066c64c80a5884fb6c476928a43add");
 
-        URL url =new URL("https://api.openweathermap.org/geo/1.0/direct?q=AR-C&limit=5&appid=3e066c64c80a5884fb6c476928a43add");
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.connect();
 
-        HttpURLConnection con =(HttpURLConnection) url.openConnection();
+        }catch(Exception e){
+
+            e.printStackTrace();
+        }
     }
 
 }
