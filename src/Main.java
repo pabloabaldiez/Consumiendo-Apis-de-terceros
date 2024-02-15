77abcb1651fdab949a134ad780e21c8d17ac147f
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-02-15 20:52:25 -0300

    zapando

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)  {
 
-try {
-    URL url = new URL("https://api.openweathermap.org/geo/1.0/direct?q=AR-C&limit=5&appid=3e066c64c80a5884fb6c476928a43add");
-    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-    con.setRequestMethod("GET");
-    con.connect();
+        try {
+            URL url = new URL("https://api.openweathermap.org/geo/1.0/direct?q=AR-C&limit=5&appid=3e066c64c80a5884fb6c476928a43add");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.connect();
 
-    int responseCode= con.getResponseCode();
+            int responseCode= con.getResponseCode();
 
-    if(responseCode != 200){
-        throw new RuntimeException("Ocurrio un error de tipo: " + responseCode);
-    } else {
+            if(responseCode != 200){
+                throw new RuntimeException("Ocurrio un error de tipo: " + responseCode);
+            } else {
 
-        StringBuilder informationString = new StringBuilder();
-        Scanner scanner= new Scanner(url.openStream());
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner= new Scanner(url.openStream());
 
-        while (scanner.hasNext()){
-            informationString.append(scanner.nextLine());
-        }
-        scanner.close();
-        System.out.println(informationString);
-    }
+                while (scanner.hasNext()){
+                    informationString.append(scanner.nextLine());
+                }
+                scanner.close();
+                System.out.println(informationString);
+            }
 
-}catch(Exception e){
-        e.printStackTrace();
-    }
+        }catch(Exception e){
+                e.printStackTrace();
+            }
     }
 }
