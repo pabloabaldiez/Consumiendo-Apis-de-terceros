2b0714b7bc92dfe2ad736dab551a9040a3a30250
Pablo Abal Diez <112098403+pabloabaldiez@users.noreply.github.com>
2024-02-15 20:51:02 -0300

    zapando

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
 
 try {
     URL url = new URL("https://api.openweathermap.org/geo/1.0/direct?q=AR-C&limit=5&appid=3e066c64c80a5884fb6c476928a43add");
-
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("GET");
     con.connect();
@@ -25,17 +24,13 @@ try {
         while (scanner.hasNext()){
             informationString.append(scanner.nextLine());
         }
-
         scanner.close();
         System.out.println(informationString);
     }
 
-
-    }catch(Exception e){
+}catch(Exception e){
 
             e.printStackTrace();
-     }
-
     }
-
+    }
 }
